af3851d367c5a70f6b5ad4f7fd8ddb82ecc8fda7
Wendel Guedes <wendelguedes@gmail.com>
2021-04-03 10:55:56 -0300

    Corrigindo url

--- src/test/java/br/cd/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/cd/wcaquino/tasks/apitest/APITest.java
@@ -11,7 +11,7 @@ public class APITest {
 	
 	@BeforeClass
 	public static void setup() {
-		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+		RestAssured.baseURI = "http://192.168.0.2:8001/tasks-backend";
 	}
 
 	@Test
